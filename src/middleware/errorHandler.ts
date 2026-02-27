af17e0a0cd6b98f7b5655c9b544940c7c34a42a5
Edilson Sérgio Budula <87162904+EdilsonBudula@users.noreply.github.com>
2026-02-27 21:54:28 +0100

    Add error handling middleware

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,15 @@
+// errorHandler.ts
+
+import { Request, Response, NextFunction } from 'express';
+
+const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
+
+    res.status(status).json({
+        status,
+        message,
+    });
+};
+
+export default errorHandler;
